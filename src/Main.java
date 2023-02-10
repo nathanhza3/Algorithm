758c2ae915a0df5d13a0f6195b3491c2de40683f
nathanhza3 <116544849+nathanhza3@users.noreply.github.com>
2023-02-10 16:41:00 +0100

    tp1

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        somme();
+        division();
+        volume();
+        Scanner scanner = new Scanner(System.in);
+        /*int unEntier = scanner.nextInt();
+        float unReel = scanner.nextFloat();
+        System.out.println("J’ai récuperé un entier: " + unEntier);
+        System.out.println("J’ai aussi récuperé un reel: " + unReel);
+
+
+        System.out.println("Bonjour, quel est votre prénom?");
+        String prenom = scanner.nextLine();
+        System.out.println("Bonjour " + prenom);*/
+    }
+
+    public static void somme () {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Veuillez saisir le premier entier");
+        System.out.println("Veuillez saisir le deuxième entier");
+
+        int premierEntier = scanner.nextInt();
+        int deuxiemeEntier = scanner.nextInt();
+        int somme = premierEntier + deuxiemeEntier;
+
+        System.out.println("La somme de " + premierEntier + " avec " + deuxiemeEntier +
+                " est égale à " + somme);
+
+
+    }
+
+    public static void division () {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Veuillez saisir le premier entier");
+        System.out.println("Veuillez saisir le deuxieme entier");
+        int premierEntier = scanner.nextInt();
+        float deuxiemeEntier = scanner.nextFloat();
+        float division = premierEntier / deuxiemeEntier;
+        System.out.println("Le quotient de " + premierEntier + " avec " + deuxiemeEntier +
+                "est egale a " + division);
+
+    }
+
+    public static void volume () {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Veuillez saisir la longueur");
+        System.out.println("Veuillez saisir la largeur");
+        System.out.println("Veuillez saisir la hauteur");
+        float longueur = scanner.nextFloat();
+        float largeur = scanner.nextFloat();
+        float hauteur = scanner.nextFloat();
+        float volume = longueur*largeur*hauteur;
+        System.out.println("Le volume du pavé "  +
+                "est égale à " + volume);
+
+    }
+
+}
